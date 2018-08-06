a09adc5b3bce4a10d177afd9115aa627659bd4f8
bob <aminebmca@hotmail.com>
2018-08-06 11:59:51 +0200

    ajouter un lien dans contact fix #1

--- contact.php
+++ contact.php
@@ -1,4 +1,5 @@
 <h1>contact</h1>
-<p>paragraphe sur master</p>
 <a href="#">Google</a>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ut quaerat, libero obcaecati possimus recusandae officia unde iusto magnam dolorem fugit ad repellendus at explicabo iste nostrum doloremque dolore excepturi.</p>
 
+<a href="#">google</a>
